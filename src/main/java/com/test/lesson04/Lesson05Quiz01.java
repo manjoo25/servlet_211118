a70c57ced77d523743b763b21f41978c8911f1de
DESKTOP-I7C294J\user <k0102joo@naver.com>
2022-02-10 23:43:46 +0900

    20220210-과제 lesson04/quiz01

--- src/main/java/com/test/lesson04/Lesson05Quiz01.java
+++ src/main/java/com/test/lesson04/Lesson05Quiz01.java
@@ -26,9 +26,14 @@ public class Lesson05Quiz01 extends HttpServlet {
 		// insert
 		String insertQuery = "insert into `real_estate` (`realtorId`, `address`, `area`, `type`, `price`, `rentPrice`)"
 				+ "values (3, '헤라펠리스 101동 5305호', 350, '매매', 1500000, NULL);";
+		try {
+			mysql.update(insertQuery);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 		
 		// select
-		String selectQuery = "select * from `real_estate` limit 10";
+		String selectQuery = "select * from `real_estate` order by `id` desc limit 10";
 		
 		// 결과 출력
 		try {
